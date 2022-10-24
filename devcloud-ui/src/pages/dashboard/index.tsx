18286117ed162e22ad21d2f37e1b58beb2af053e
Moritz Eck <moritz.eck@gmail.com>
2022-10-24 16:39:14 +0200

    feat: integrate auth0 login/logout

--- devcloud-ui/src/pages/dashboard/index.tsx
+++ devcloud-ui/src/pages/dashboard/index.tsx
@@ -1,11 +1,26 @@
+import { Claims, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import type { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../../features/layout/Layout";
 
-export const Dashboard: NextPage = () => {
+export const Dashboard: NextPage<Claims> = ({ user }) => {
   return (
-    <div>
-      <h1 className="text-2xl">Dashboard</h1>
-    </div>
+    <Layout>
+      <main className="w-full px-4 py-6 mx-auto lg:w-4/12">
+        <h1 className="mb-3 text-2xl font-bold text-center lg:text-3xl">Welcome to the Devcloud</h1>
+        <div className="px-6">
+          <div className="w-full mb-3 rounded-lg shadow-lg bg-slate-200">
+            <p className="p-3">Welcome, {user.name}</p>
+          </div>
+          <Link href="/api/auth/logout">
+            <div className="px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-slate-800 active:bg-slate-600 hover:shadow-lg focus:outline-none">
+              <a>Logout</a>
+            </div>
+          </Link>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
-export default Dashboard;
+export default withPageAuthRequired(Dashboard);
--- devcloud-ui/src/pages/index.tsx
+++ devcloud-ui/src/pages/index.tsx
@@ -1,41 +1,33 @@
+import { useUser } from "@auth0/nextjs-auth0";
 import type { NextPage } from "next";
-import FormInput from "../features/startpage/FormInput";
-import GoogleSignInButton from "../features/startpage/GoogleSignInButton";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Layout from "../features/layout/Layout";
 
 export const Home: NextPage = () => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  if (user) {
+    router.push("/dashboard");
+  }
+
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-slate-50">
-      <h1 className="text-3xl font-bold lg:text-4xl">Welcome to the Devcloud</h1>
-      <div className="w-full px-4 py-6 mx-auto lg:w-4/12">
-        <div className="flex flex-col w-full border-0 rounded-lg shadow-lg bg-slate-200">
-          <div className="p-6">
-            <div className="mb-3 text-center">
-              <h6 className="text-sm font-bold text-slate-500">Sign in with</h6>
-            </div>
-            <GoogleSignInButton />
+    <Layout>
+      <main className="w-full px-4 py-6 mx-auto lg:w-4/12">
+        <h1 className="mb-3 text-2xl font-bold text-center lg:text-3xl">Welcome to the Devcloud</h1>
+        <div className="px-6">
+          <div className="w-full mb-3 rounded-lg shadow-lg bg-slate-200">
+            <p className="p-3">Welcome, please login first!</p>
           </div>
-          <hr className="border-b-1 border-slate-300" />
-          <div className="flex-auto px-4 pt-6 pb-10 lg:px-10">
-            <div className="mb-3 text-center">
-              <p className="text-sm font-bold text-slate-500">Or sign in with credentials</p>
+          <Link href="/api/auth/login">
+            <div className="px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-slate-800 active:bg-slate-600 hover:shadow-lg focus:outline-none">
+              <a>Login</a>
             </div>
-            <form>
-              <FormInput label="Email" type="email" placeholder="Email" autocomplete="username" />
-              <FormInput label="Password" type="password" placeholder="Password" autocomplete="current-password" />
-              <div className="pt-3 text-center">
-                {/* TODO: maybe refactor button into separate componentr */}
-                <button
-                  className="w-full px-6 py-3 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-slate-800 active:bg-slate-600 hover:shadow-lg focus:outline-none"
-                  type="button"
-                >
-                  Sign In
-                </button>
-              </div>
-            </form>
-          </div>
+          </Link>
         </div>
-      </div>
-    </section>
+      </main>
+    </Layout>
   );
 };
 
