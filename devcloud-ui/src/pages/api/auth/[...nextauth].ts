646a6ee0427a4cc7fd6f7cbe2b184213cb31c210
Moritz Eck <moritz.eck@gmail.com>
2022-11-13 15:32:41 +0100

    fix: next-auth setup

--- devcloud-ui/src/pages/api/auth/[...nextauth].ts
+++ devcloud-ui/src/pages/api/auth/[...nextauth].ts
@@ -5,9 +5,10 @@ import Auth0Provider from "next-auth/providers/auth0";
 export const authOptions: NextAuthOptions = {
   providers: [
     Auth0Provider({
-      clientId: process.env.AUTH0_CLIENT_ID || "",
-      clientSecret: process.env.AUTH0_CLIENT_SECRET || "",
+      clientId: process.env.AUTH0_CLIENT_ID || "AUTH0_CLIENT_ID -> not present",
+      clientSecret: process.env.AUTH0_CLIENT_SECRET || "AUTH0_CLIENT_SECRET -> not present",
       issuer: process.env.AUTH0_ISSUER_BASE_URL,
+      authorization: { params: { scope: "openid" } },
       idToken: true,
     }),
   ],
